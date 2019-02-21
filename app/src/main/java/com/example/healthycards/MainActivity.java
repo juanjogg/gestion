e4ccc2328ca5620eb75942617e327dd71ff6e124
Sebastian Loaiza <sebas78915@hotmail.com>
2019-02-21 18:35:48 -0500

    Registro y olvidar contraseña

--- app/src/main/java/com/example/healthycards/MainActivity.java
+++ app/src/main/java/com/example/healthycards/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -17,11 +18,35 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private GoogleApiClient apiClient;
     private SignInButton signInBtn;
+    TextView registro;
+    TextView olvPsswd;
+
     public static final int SIGN_IN_CODE = 777;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registro = (TextView)findViewById(R.id.txtRegistro);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registro = new Intent(MainActivity.this,RegistrarActivity.class);
+                startActivity(registro);
+            }
+        });
+
+        olvPsswd = (TextView)findViewById(R.id.tvOlvContr);
+
+        olvPsswd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent olvPsswd = new Intent(MainActivity.this,OlvidarPsswdActivity.class);
+                startActivity(olvPsswd);
+            }
+        });
+
         signInBtn = findViewById(R.id.signInButton);
         GoogleSignInOptions gsOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
 
---
+++ app/src/main/java/com/example/healthycards/OlvidarPsswdActivity.java
@@ -0,0 +1,13 @@
+package com.example.healthycards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OlvidarPsswdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.olvidar_contrasena);
+    }
+}
