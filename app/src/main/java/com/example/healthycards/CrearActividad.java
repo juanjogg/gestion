0bfce06a87b070549ef8ff0486e9ff71b5946b41
Juan David Goez Yepes <juan.goez@upb.edu.co>
2019-03-08 17:03:13 -0500

    Creación de spinner dificultades, creación boton agg imagen, cambio a TextBox: dificultad actividad

--- app/src/main/java/com/example/healthycards/CrearActividad.java
+++ app/src/main/java/com/example/healthycards/CrearActividad.java
@@ -3,22 +3,37 @@ package com.example.healthycards;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CrearActividad extends AppCompatActivity {
 
     private EditText nombreAct, etDescripcion, etDificultad, etTiempo;
     private Button btnEnviar;
 
+    Spinner comboDificultad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crear_actividad);
 
+
+        comboDificultad = findViewById(R.id.spinDifiActividad);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.combo_dificultades, android.R.layout.simple_spinner_item);
+        comboDificultad.setAdapter(adapter);
+
+
         nombreAct = findViewById(R.id.txtnombreActividad);
         etDescripcion = findViewById(R.id.txtdescActividad);
-        etDificultad = findViewById(R.id.txtdifiActividad);
+
+        //OJO: Hacer casteo acá para poder leer el dato.
+        //etDificultad = findViewById(R.id.spinDifiActividad);
+
         etTiempo = findViewById(R.id.txttiempoActividad);
     }
+
+
 }
